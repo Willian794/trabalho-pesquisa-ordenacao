db39ea213b6b49045360ce0947d4b450eeb80a8e
Willian Alves <alveswillian794@gmail.com>
2024-11-21 00:24:07 -0300

    Adição do método comparar na classe Ordenador, que permite ordernar um ConjuntoMultas pela data e pelo horário, caso a placa for igual

--- models/Ordenador.java
+++ models/Ordenador.java
@@ -3,7 +3,7 @@ package models;
 public class Ordenador
 {
 	// Método de Ordenção HeapSort
-	public static void heapSort(ConjuntoMultas multas) 
+	public static void ordenarComHeapSort(ConjuntoMultas multas)
 	{
 		int n = multas.getQuant();
 
@@ -23,16 +23,43 @@ public class Ordenador
 		int esquerda = 2 * i + 1;
 		int direita = 2 * i + 2;
 
-		if (esquerda < n && multas.get(esquerda).getPlaca().compareTo(multas.get(maior).getPlaca()) > 0)
+		if (esquerda < n && comparar(multas.get(esquerda),multas.get(maior)) > 0)
+		//if (esquerda < n && multas.get(esquerda).getPlaca().compareTo(multas.get(maior).getPlaca()) > 0)
 			maior = esquerda;
 
-		if (direita < n && multas.get(direita).getPlaca().compareTo(multas.get(maior).getPlaca()) > 0)
+		if (direita < n && comparar(multas.get(direita),multas.get(maior)) > 0)
+		//if (direita < n && multas.get(direita).getPlaca().compareTo(multas.get(maior).getPlaca()) > 0)
 			maior = direita;
 
 		if (maior != i) 
 		{
 			multas.trocar(i, maior);
 			refazHeap(multas, n, maior);
+		}
+	}
+	
+	private static int comparar(Multa multa1, Multa multa2)
+	{
+		String placa1 = multa1.getPlaca(); 
+		String placa2 = multa2.getPlaca();
+		int comparacaoPlaca = placa1.compareTo(placa2); 
+		
+		if (comparacaoPlaca != 0)
+			return comparacaoPlaca;
+		else 
+		{
+			Data data1 = multa1.getData();
+			Data data2 = multa2.getData();
+			int comparacaoData = data1.compareTo(data2);
+			
+			if (comparacaoData != 0)
+				return comparacaoData;
+			else
+			{
+				Horario horario1 = multa1.getHorario();
+				Horario horario2 = multa2.getHorario();
+				return horario1.compareTo(horario2);
+			}
 		}
 	}
 }
