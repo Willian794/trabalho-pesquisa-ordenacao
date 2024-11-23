9535a0ee044b58553366523c61cf69e038caefdc
Willian Alves <alveswillian794@gmail.com>
2024-11-23 15:27:15 -0300

    Classes dos exércicios 3 e 4 feitas

---
+++ main/Atividade3.java
@@ -0,0 +1,35 @@
+package main;
+
+import java.io.IOException;
+
+import models.Arquivo;
+import models.ColecaoMultas;
+import models.Cronometrador;
+import models.Cronometrador.MedidaTempo;
+import models.Multa;
+import models.Ordenador;
+
+public class Atividade3 
+{
+	public static void main(String[] args) throws IOException
+	{
+		ColecaoMultas multas = new ColecaoMultas(new Arquivo("dados/originais/multa20alea.txt"));
+		Cronometrador cronometrador = new Cronometrador();
+		
+		cronometrador.iniciar();
+		
+		Ordenador.ordenarComQuickSortInsercaoDireta(multas);
+		
+		int count = 0;
+		for (Multa multa : multas)
+		{
+			System.out.println(multa);
+			count++;
+		}
+		
+		cronometrador.finalizar();
+		
+		System.out.println("\nTempo para ordenar: " + cronometrador.getDeltaTempo(MedidaTempo.SEGUNDOS) + " segundos.");
+		System.out.println("Contador: " + count);
+	}
+}
