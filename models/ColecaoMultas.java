e748972936ad54f2cfdf99af5ec22435e443795e
Willian Alves <alveswillian794@gmail.com>
2024-11-23 15:14:47 -0300

    Classe ConjuntoMultas renomeada para ColecaoMultas

---
+++ models/ColecaoMultas.java
@@ -0,0 +1,91 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ColecaoMultas implements Colecao<Multa>
+{
+	private ArrayList<Multa> multas;
+	private int quant;
+	
+	public ColecaoMultas(Arquivo arquivo)
+	{
+		this.multas = new ArrayList<>();
+		this.quant = 0;
+		ArrayList<String> linhas = arquivo.lerLinhas();
+		String[] vetCampos;
+		for (String linha : linhas)
+		{
+			vetCampos = linha.split(";");
+			multas.add(
+				new Multa(
+					vetCampos[0],
+					vetCampos[1],
+					vetCampos[2],
+					new Data(vetCampos[3]),
+					new Horario(vetCampos[4])
+				)
+			);
+			this.quant++;
+		}
+	}
+
+	@Override
+	public Iterator<Multa> iterator() {
+		return new ColecaoMultasIterator(this.multas);
+	}
+
+	@Override
+	public boolean add(Multa elem) {
+		return this.multas.add(elem);
+	}
+
+	@Override
+	public boolean add(int indice, Multa elem) {
+		if (indice >= this.quant || indice < 0)
+			return false;
+		this.quant++;
+		this.multas.add(indice, elem);
+		return true;
+	}
+
+	@Override
+	public int size() {
+		return this.quant;
+	}
+
+	@Override
+	public Multa get(int indice) {
+		if (indice >= quant || indice < 0)
+			return null;
+		
+		return multas.get(indice);
+	}
+
+	@Override
+	public boolean set(int indice, Multa elem) {
+		if (indice >= quant || indice < 0)
+			return false;
+		multas.remove(indice);
+		multas.add(indice, elem);
+		return true;
+	}
+
+	@Override
+	public boolean trocar(int indice1, int indice2) {
+		if ((indice1 >= this.quant || indice1 < 0) || (indice2 >= this.quant || indice2 < 0))
+			return false;
+		Multa multa = multas.get(indice1);
+		this.set(indice1, this.get(indice2));
+		this.set(indice2, multa);
+		return true;
+	}
+
+	@Override
+	public Multa remove(int indice) {
+		if (indice >= quant || indice < 0)
+			return null;
+		quant--;
+		return multas.remove(indice);
+	}
+}
--- models/ColecaoMultasIterator.java
+++ models/ColecaoMultasIterator.java
@@ -3,12 +3,12 @@ package models;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class ConjuntoMultasIterator implements Iterator<Multa>
+public class ColecaoMultasIterator implements Iterator<Multa>
 {
 	private int indice;
 	private ArrayList<Multa> multas;
 	
-	public ConjuntoMultasIterator(ArrayList<Multa> multas)
+	public ColecaoMultasIterator(ArrayList<Multa> multas)
 	{
 		this.indice = -1;
 		this.multas = multas;
