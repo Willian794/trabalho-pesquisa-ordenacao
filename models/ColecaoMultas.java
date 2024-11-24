ecc93cd3a59185b24f9c0b4fc98f9fe98c87c75f
Willian Alves <alveswillian794@gmail.com>
2024-11-24 10:23:33 -0300

    Atualizado o caminho da interface na classe ColecaoMultas

--- models/ColecaoMultas.java
+++ models/ColecaoMultas.java
@@ -3,6 +3,8 @@ package models;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import models.interfaces.Colecao;
+
 public class ColecaoMultas implements Colecao<Multa>
 {
 	private ArrayList<Multa> multas;
